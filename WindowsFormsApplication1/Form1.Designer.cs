f14a97f35be07a99325f72c1b66b171922fdabcb
LZY <lizeyu0325@qq.com>
2017-08-09 16:19:26 +0800

    代码实现word替换文本，拆分文件的功能

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WordRedevelop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "文本替换";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(74, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "word拆分";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordRedevelop
+{
+    public partial class Form1 : Form
+    {
+
+        object saveFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ApplicationClass word = new Microsoft.Office.Interop.Word.ApplicationClass();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExchangeText();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CovertWord();
+        }
+
+        /// <summary>
+        /// 替换文本
+        /// </summary>
+        private void ExchangeText()
+        {
+            Microsoft.Office.Interop.Word.Application app = null;
+            Document doc = null;
+
+            try
+            {
+                object missing = System.Reflection.Missing.Value;
+                object fileName = @"C:\Users\DELL\Desktop\松轩可研说明-输变电工程.docx";
+                saveFile = @"C:\Users\DELL\Desktop\松轩可研说明-输变电工程（修改）.docx";
+                app = new Microsoft.Office.Interop.Word.Application();
+                doc = app.Documents.Open(ref fileName, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+
+                Dictionary<string, string> datas = new Dictionary<string, string>();
+                datas.Add("上海金山松轩110kV输变电工程", "哈哈哈哈哈");
+
+                object replace = WdReplace.wdReplaceAll;
+                foreach (var item in datas)
+                {
+                    app.Selection.Find.Replacement.ClearFormatting();
+                    app.Selection.Find.ClearFormatting();
+                    app.Selection.Find.Text = item.Key;
+                    app.Selection.Find.Replacement.Text = item.Value;
+
+                    app.Selection.Find.Execute(
+                    ref missing, ref missing,
+                    ref missing, ref missing,
+                    ref missing, ref missing,
+                    ref missing, ref missing, ref missing,
+                    ref missing, ref replace,
+                    ref missing, ref missing,
+                    ref missing, ref missing);
+                }
+
+                doc.SaveAs2(saveFile, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                if (doc != null)
+                    doc.Close();
+                if (app != null)
+                    app.Quit();
+                MessageBox.Show("替换成功！");
+            }
+
+        }
+
+        /// <summary>
+        /// 拆分word
+        /// </summary>
+        private void CovertWord()
+        {
+            object missing = System.Reflection.Missing.Value;
+
+            try
+            {
+                string path = @"C:\Users\DELL\Desktop\split";
+                //Directory.CreateDirectory(path);
+
+                //Document doc = ReadDocument(@"C:\Users\DELL\Desktop\示例word.doc");
+                Document doc = ReadDocument((string)saveFile);
+                      
+
+                int[,] positions = GetPosition(doc);
+
+
+                object oStart = doc.Content.Start;
+                object oEnd = 0;
+                for (int i = 0; i < doc.Bookmarks.Count; i++)
+                {
+                    if (i != doc.Bookmarks.Count-1)
+                    {
+                        oEnd = positions[i, 0];
+                    }
+                    else
+                    {
+                        oEnd = doc.Content.End;
+                    }
+
+                    Range tocopy = doc.Range(ref oStart, ref oEnd);
+                    tocopy.Copy();
+
+                    Document docto = CreateDocument();
+                    docto.Content.PasteAndFormat(WdRecoveryType.wdFormatOriginalFormatting);
+
+                    object filename = path + @"\" + "test" + i.ToString() + ".docx";
+                    object format = WdSaveFormat.wdFormatDocumentDefault; 
+                    docto.SaveAs(ref filename, ref format, ref missing, ref missing, ref missing,
+                        ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                        ref missing, ref missing, ref missing, ref missing, ref missing);
+                    docto.Close(ref missing, ref missing, ref missing);
+
+                    oStart = oEnd;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                word.Quit(ref missing, ref missing, ref missing);
+                MessageBox.Show("拆分成功!");
+            }
+
+
+        }
+
+        /// <summary>
+        /// 合并word
+        /// </summary>
+        private void MergeWord()
+        {
+
+        }
+
+        #region Helper Function
+        /// <summary>
+        /// 创建word文档
+        /// </summary>
+        /// <returns></returns>
+        private Document CreateDocument()
+        {
+            object missing = System.Reflection.Missing.Value;
+            Document newdoc = word.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            return newdoc;
+        }
+
+        /// <summary>
+        /// 读取word文档
+        /// </summary>
+        /// <param name="path">word文档路径</param>
+        /// <returns></returns>
+        private Document ReadDocument(string path)
+        {
+            object missing = System.Reflection.Missing.Value;
+
+            Type wordType = word.GetType();
+
+            Documents docs = word.Documents;
+            Type docsType = docs.GetType();
+            object objDocName = path;
+            Document doc = (Document)docsType.InvokeMember("Open", System.Reflection.BindingFlags.InvokeMethod, null, docs, new Object[] { objDocName, true, true });
+
+            return doc;
+        }
+
+        /// <summary>
+        /// 获取书签的位置
+        /// </summary>
+        /// <param name="word">文档对象</param>
+        /// <returns></returns>
+        private int[,] GetPosition(Document word)
+        {
+            
+            int bmcount = word.Bookmarks.Count;
+            int[,] result = new int[bmcount, 2];
+            for (int i = 1; i <= bmcount; i++)
+            {
+                object index = i;
+                Bookmark bm = word.Bookmarks.get_Item(ref index);
+                result[i-1, 0] = bm.Start;
+                result[i-1, 1] = bm.End;
+                
+            }
+            return result;
+        }
+        #endregion
+
+
+    }
+}
